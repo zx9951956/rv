cce57f7c9a11fb40ddddad50c9000cb8f1d1945b
鄭宇翔 <kk476523asd@gmail.com>
2020-07-07 17:37:54 +0800

    問卷功能完成

--- questionnaire.js
+++ questionnaire.js
@@ -16,6 +16,7 @@ $('#submit').on('click', function () {
         }
         return temp;
     };
+    //年齡
     var year = function () {
         var temp;
         $('[name="year"]').each(function () {
@@ -26,16 +27,122 @@ $('#submit').on('click', function () {
         }
         return temp;
     };
+     //居住地
+     var place = function () {
+        var temp;
+        $('[name="place"]').each(function () {
+            if ($(this).prop('checked') === true) temp = $(this).val();
+        });
+        if (temp == null) {
+            state = 1
+        }
+        return temp;
+    };
+    //遊玩次數
+    var time = function () {
+        var temp;
+        $('[name="time"]').each(function () {
+            if ($(this).prop('checked') === true) temp = $(this).val();
+        });
+        if (temp == null) {
+            state = 1
+        }
+        return temp;
+    };
+    var exhibitionHall = function () {
+        var temp;
+        $('[name="exhibitionHall"]').each(function (i) {
+                if ($(this).prop('checked') === true){
+                    if(i ==0 ){
+                        temp = $(this).val();
+                    }
+                    else{
+                        if(temp == null){
+                            temp = $(this).val();
+                        }
+                        else{
+                            temp += (',' +  $(this).val());
+                        }
+                    }
+                }
+            
+        });
+        if (temp == null) {
+            state = 1
+        }
+        return temp;
+    };
+    //是否再來
+    var come = function () {
+        var temp;
+        $('[name="come"]').each(function () {
+            if ($(this).prop('checked') === true) temp = $(this).val();
+        });
+        if (temp == null) {
+            state = 1
+        }
+        return temp;
+    };
+    //是否聽懂導覽
+    var understand = function () {
+        var temp;
+        $('[name="understand"]').each(function () {
+            if ($(this).prop('checked') === true) temp = $(this).val();
+        });
+        if (temp == null) {
+            state = 1
+        }
+        return temp;
+    };
+     //是否分享
+     var share = function () {
+        var temp;
+        $('[name="share"]').each(function () {
+            if ($(this).prop('checked') === true) temp = $(this).val();
+        });
+        if (temp == null) {
+            state = 1
+        }
+        return temp;
+    };
+     //是否有趣
+     var interesting = function () {
+        var temp;
+        $('[name="interesting"]').each(function () {
+            if ($(this).prop('checked') === true) temp = $(this).val();
+        });
+        if (temp == null) {
+            state = 1
+        }
+        return temp;
+    };
+    //建議
+    var suggest = function () {
+        var temp = $('[name="suggest"]').val();
+        if (temp == "") {
+            state = 1
+        }
+        return temp;
+    };
     // post
     var data = {
-        'entry.781046923': sex(),
-        'entry.423309942': year()
+        'entry.678731899': sex(),
+        'entry.985182338': year(),
+        'entry.299271935':place(),
+        'entry.1801503054':time(),
+        'entry.1739358294':exhibitionHall(),
+        'entry.509556683':come(),
+        'entry.377136009':understand(),
+        'entry.859527971':share(),
+        'entry.1560278706':interesting(),
+        'entry.1950067070':suggest()
+        
     };
 
     if (state == 0) {
         $.ajax({
             type: 'POST',
-            url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdyXi8kZGfp2Nz-pQp7_Ehnpi_6tc9RVDJ2fgtw3MFlEA1a8w/formResponse',
+            url: 'https://docs.google.com/forms/u/1/d/e/1FAIpQLSfEJDd1XawO8NXaYVgR6rtUqQkBJvs9eM09iE0o6nwsOp89tA/formResponse',
             data: data,
             contentType: 'application/json',
             dataType: 'jsonp',
@@ -43,6 +150,9 @@ $('#submit').on('click', function () {
                 alert('資料已送出！');
             }
         });
+    }
+    else{
+        console.log('test')
     }
 
 });
