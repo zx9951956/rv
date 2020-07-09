90c9ceb8f394464c9f4a75de30e28cbfe9f1775c
鄭宇翔 <kk476523asd@gmail.com>
2020-07-09 18:13:35 +0800

    no message

--- questionnaire.js
+++ questionnaire.js
@@ -1,3 +1,25 @@
+window.onload = function() {
+    // 阻止雙擊放大
+    var lastTouchEnd = 0;
+    document.addEventListener('touchstart', function(event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    });
+    document.addEventListener('touchend', function(event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        }
+        lastTouchEnd = now;
+    }, false);
+
+    // 阻止雙指放大
+    document.addEventListener('gesturestart', function(event) {
+        event.preventDefault();
+    });
+}
+
 var btnLeft = document.getElementById('button-left')
 var btnRight = document.getElementById('button-right')
 var progressBar = document.getElementById('progressBar')
